f5a907881b9a191b89452f5785b08dae3ebe5618
psx95 <pranav.ps95@hotmail.com>
2019-03-04 00:43:06 +0530

    feat(ViewController): Adds code to take pictures and set them to ImageView

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -14,13 +14,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imageView: UIImageView!
     
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        } else {
+            fatalError("Cannot load image")
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
+        present(imagePicker, animated: true, completion: nil)
     }
     
 }
